f37efb3e9134097eeb28fa398908b5af34148978
xenacode-art <kaja.obinna@lmu.edu.ng>
2022-02-19 22:48:12 +0100

    Started working with serializers

--- Rest_Framework/settings.py
+++ Rest_Framework/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restapi',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- restapi/views.py
+++ restapi/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+# import local data
 # Create your views here.
+# Create  a viewset
+from .serializers import GeeksSerializer
+from rest_framework import viewsets
+from .models import GeeksModel
+
+
+class GeekViewSet(viewsets.ModelViewSet):
+    # define queryset
+    queryset = GeeksModel.object.all()
+
+    # specify serializer to be used
+    serializer_class = GeeksSerializer
